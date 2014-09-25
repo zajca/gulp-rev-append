78140dd04fd89d159cf64a9cc2997d2da5a8b6c3
Martin Zajic <zajic@priasystem.cz>
2014-09-25 11:51:59 +0200

    update any rev number is just random hash

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var map = require('event-stream').map;
 
-var FILE_DECL = /(?:href|src)="(.*)[\?]rev=(.*)[\"]/gi;
+var FILE_DECL = /[\?]rev=(.*)[\"]/gi;
+
+function randomValueHex (len) {
+    return crypto.randomBytes(Math.ceil(len/2))
+        .toString('hex')
+        .slice(0,len);
+}
 
 var revPlugin = function revPlugin() {
 
@@ -17,32 +23,29 @@ var revPlugin = function revPlugin() {
     var i, length = lines.length;
     var line;
     var groups;
-    var dependencyPath;
-    var data, hash;
+    var hash;
 
     if(!file) {
       throw new PluginError('gulp-rev', 'Missing fileName option for gulp-rev.');
     }
+    hash = randomValueHex(5)
 
     for(i = 0; i < length; i++) {
       line = lines[i];
       groups = FILE_DECL.exec(line);
       if(groups && groups.length > 1) {
-        dependencyPath = path.resolve(path.dirname(file.path), groups[1]);
         try {
-          data = fs.readFileSync(dependencyPath);
-          hash = crypto.createHash('md5');
-          hash.update(data.toString(), 'utf8');
-          line = line.replace(groups[2], hash.digest('hex'));
+            line = line.replace(groups[1], hash);
         }
         catch(e) {
           // fail silently.
+            console.log("gulp-rev failed",e);
         }
       }
       lines[i] = line;
       FILE_DECL.lastIndex = 0;
     }
-
+    //console.log(lines)
     file.contents = new Buffer(lines.join('\n'));
     cb(null, file);
 
